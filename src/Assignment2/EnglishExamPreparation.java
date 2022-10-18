b53a5eba4d7fd5c655605fdf0a25beb3041352a4
tyk1304245946 <50909974+tyk1304245946@users.noreply.github.com>
2022-10-18 17:54:49 +0800

    Update EnglishExamPreparation.java

--- src/Assignment2/EnglishExamPreparation.java
+++ src/Assignment2/EnglishExamPreparation.java
@@ -8,27 +8,48 @@ public class EnglishExamPreparation {
         Scanner input = new Scanner(System.in);
 
         int n = input.nextInt();
-        double[] scores = new double[4];
-        int[] day = new int[4];
+
+        int[][] matrix = new int[2][4];
 
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 4; j++) scores[j] = input.nextInt();
-            for (int j = 0; j < 4; j++) day[j] = input.nextInt();
-
+        for (int k = 0; k < n; k++) {
             int x = 0;
-            while (Arrays.stream(scores).sum() < 100) {
-                x++;
-                for (int j = 0; j < 4; j++) {
-                    if (scores[j] + 1.0 / day[j] <= 30) {
-                        scores[j] += 1.0 / day[j];
+            for (int i = 0; i < 4; i++)
+                matrix[0][i] = input.nextInt();
+            for (int i = 0; i < 4; i++)
+                matrix[1][i] = input.nextInt();
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = i + 1; j < 4; j++) {
+                    if (matrix[1][i] > matrix[1][j]) {
+                        int temp = matrix[0][i];
+                        matrix[0][i] = matrix[0][j];
+                        matrix[0][j] = temp;
+
+                        temp = matrix[1][i];
+                        matrix[1][i] = matrix[1][j];
+                        matrix[1][j] = temp;
                     }
-                    else{
-                        scores[j] = 30;
-                    }
+                }
+            }
+
+            int sum = Arrays.stream(matrix[0]).sum();
+            if (sum>=100){
+                System.out.println(0);
+                continue;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                if (sum + 30 - matrix[0][i] >= 100) {
+                    x += (100 - sum) * matrix[1][i];
+                    break;
+                } else {
+                    x += (30 - matrix[0][i]) * matrix[1][i];
+                    sum += 30 - matrix[0][i];
                 }
             }
             System.out.println(x);
         }
     }
 }
+
