24101af2bccd87e77af1dc80baa9ad8b30afcfff
tyk1304245946 <50909974+tyk1304245946@users.noreply.github.com>
2022-10-18 17:08:17 +0800

    EnglishExamPreparation_Attempt1

--- src/Assignment2/EnglishExamPreparation.java
+++ src/Assignment2/EnglishExamPreparation.java
@@ -1,4 +1,34 @@
 package Assignment2;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class EnglishExamPreparation {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        double[] scores = new double[4];
+        int[] day = new int[4];
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 4; j++) scores[j] = input.nextInt();
+            for (int j = 0; j < 4; j++) day[j] = input.nextInt();
+
+            int x = 0;
+            while (Arrays.stream(scores).sum() < 100) {
+                x++;
+                for (int j = 0; j < 4; j++) {
+                    if (scores[j] + 1.0 / day[j] <= 30) {
+                        scores[j] += 1.0 / day[j];
+                    }
+                    else{
+                        scores[j] = 30;
+                    }
+                }
+            }
+            System.out.println(x);
+        }
+    }
 }
--- src/Assignment2/MissingNumbersInAnArray.java
+++ src/Assignment2/MissingNumbersInAnArray.java
@@ -1,4 +1,35 @@
 package Assignment2;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MissingNumbersInAnArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        boolean[] array = new boolean[n];
+        Arrays.fill(array, false);
+
+        for (int i = 0; i < n; i++) {
+            int a = input.nextInt();
+            array[a-1] = true;
+        }
+
+        boolean flag = false;
+        for (int i = 0; i < n; i++) {
+            if (!array[i]) {
+                if (!flag) {
+                    System.out.printf("%d", i + 1);
+                    flag = true;
+                } else {
+                    System.out.printf(" %d", i + 1);
+                }
+            }
+        }
+        if (!flag) {
+            System.out.println("NULL");
+        }
+
+    }
 }
