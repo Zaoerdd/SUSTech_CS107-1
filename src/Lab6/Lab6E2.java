2a13b8545b7c84f198451d515392fc218767643c
tyk1304245946 <50909974+tyk1304245946@users.noreply.github.com>
2022-10-19 21:03:37 +0800

    Create Lab6E2.java

---
+++ src/Lab6/Lab6E2.java
@@ -0,0 +1,21 @@
+package Lab6;
+
+import java.util.Scanner;
+
+public class Lab6E2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please input three numbers for bottom and height:");
+        double bottom = input.nextDouble();
+        double height = input.nextDouble();
+        System.out.printf("The area is %.3f\n", MyTriangle.area(bottom, height));
+
+        System.out.println("Please input two numbers for a and b:");
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        System.out.println("Please input a number in (0, 180) for angle (angle is a float variable):");
+        int angleOfAAndB = input.nextInt();
+        System.out.printf("The area is %.3f\n", MyTriangle.area(a, b, angleOfAAndB));
+    }
+}
