93c26397350185a7aa86e0c739e524ffe8d1a6d1
tyk1304245946 <50909974+tyk1304245946@users.noreply.github.com>
2022-10-19 20:51:43 +0800

    Update LittleendianAndBigendian.java

--- src/Assignment2/LittleendianAndBigendian.java
+++ src/Assignment2/LittleendianAndBigendian.java
@@ -1,4 +1,27 @@
 package Assignment2;
 
+import java.util.Scanner;
+
 public class LittleendianAndBigendian {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String str = input.next();
+            System.out.print(str.charAt(6));
+            System.out.print(str.charAt(7));
+            System.out.print(str.charAt(4));
+            System.out.print(str.charAt(5));
+            System.out.print(str.charAt(2));
+            System.out.print(str.charAt(3));
+            System.out.print(str.charAt(0));
+            System.out.print(str.charAt(1));
+            System.out.print(" ");
+
+        }
+
+
+    }
 }
