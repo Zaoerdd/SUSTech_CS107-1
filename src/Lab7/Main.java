c3603a6fcb284a4d337b18ceb95a599e2ec76764
tyk1304245946 <50909974+tyk1304245946@users.noreply.github.com>
2022-10-25 16:02:38 +0800

    Create Main.java

---
+++ src/Lab7/Main.java
@@ -0,0 +1,22 @@
+package Lab7;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static Lab7.SoftOpening.*;
+
+public class Main {
+
+    public static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+        ArrayList<Food> foodList = generateMenu();
+        User user = generateUser();
+        user.introduce();
+        userConsume(foodList,user);
+        user.introduce();
+
+        in.close();
+    }
+}
