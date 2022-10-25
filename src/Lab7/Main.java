83084324844641478304c2c9a984520dc51178f7
tyk1304245946 <50909974+tyk1304245946@users.noreply.github.com>
2022-10-25 16:03:40 +0800

    Update Main.java

--- src/Lab7/Main.java
+++ src/Lab7/Main.java
@@ -2,21 +2,16 @@ package Lab7;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
 import static Lab7.SoftOpening.*;
 
 public class Main {
-
     public static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-
-
         ArrayList<Food> foodList = generateMenu();
         User user = generateUser();
         user.introduce();
-        userConsume(foodList,user);
+        userConsume(foodList, user);
         user.introduce();
-
         in.close();
     }
 }
