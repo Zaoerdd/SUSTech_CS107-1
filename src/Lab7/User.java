052ab2a9f533d66b093c55cc1b018d892b69ef5f
tyk1304245946 <50909974+tyk1304245946@users.noreply.github.com>
2022-10-25 14:58:13 +0800

    Create User.java

---
+++ src/Lab7/User.java
@@ -0,0 +1,61 @@
+package Lab7;
+
+import java.util.Scanner;
+
+public class User {
+    private String account;
+    private String password;
+    private double money;
+
+    public void setUser(String account) {
+        this.account = account;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    private boolean match(String password) {
+        return (password == this.password);
+    }
+
+    public void introduce() {
+        System.out.printf("%s 's account has a balance of %.2f dollar\n", account, money);
+    }
+
+    public void deposit(double money){
+        System.out.printf("Got %.2f as income, balance is %.2f dollar\n", money, this.money+money);
+        this.money += money;
+    }
+
+    public void withdraw(double money) {
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Plan to withdraw %.2f dollar, ", money);
+        if (money > this.money) {
+            System.out.println("but no sufficient balance");
+        } else {
+            System.out.println("okay");
+            System.out.println("Please input your password");
+            String password = input.next();
+            boolean flag = false;
+            for (int i = 2; i >= 0; i--) {
+                if (!flag){
+                    if (match(password)) {
+                        System.out.printf("Withdraw %.2f dollar and balance is %.2f dollar\n", money, this.money - money);
+                        this.money -= money;
+                        flag = true;
+                    }
+                    else{
+                        System.out.printf("password error, there are %d left to try\n", i);
+                    }
+                }
+            }
+        }
+    }
+
+
+}
